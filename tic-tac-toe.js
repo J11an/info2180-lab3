eae08d497b7b960e67b1737a2b6a508e926ad0f7
J.Wan <jaysun1w@gmail.com>
2021-10-09 13:31:28 -0500

    Board layout was added

---
+++ tic-tac-toe.js
@@ -0,0 +1,15 @@
+window.onload = function(){
+
+    let board = document.querySelectorAll("#board div");
+    
+    for (let x=0; x < 9; x++){
+
+        board[x].classList.add("square");
+
+    }
+
+
+
+
+
+}   
