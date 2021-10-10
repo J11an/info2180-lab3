92862ee729c4851ff84b5dc747ac703c0219103b
J.Wan <jaysun1w@gmail.com>
2021-10-09 19:35:54 -0500

    Turn system (Question 2) was added

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,12 +1,37 @@
 window.onload = function(){
 
+    let turn = "P1";
+    let gameState = ["","","","","","","","",""]
     let board = document.querySelectorAll("#board div");
-    
+
     for (let x=0; x < 9; x++){
 
         board[x].classList.add("square");
 
+        board[x].addEventListener("click",function(){
+
+            if (turn == "P1"){
+
+                board[x].textContent = "X";
+                board[x].classList.add("X");
+                turn = "P2";
+                gameState[x] = "X"
+
+            }
+
+            else if (turn == "P2"){
+
+                board[x].textContent = "O";
+                board[x].classList.add("O");
+                turn = "P1";
+                console.log(gameState);
+                gameState[x] = "Y"
+            }
+        
+
+        })
     }
+
 
 
 
